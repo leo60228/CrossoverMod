a22c5fc7e670a775d48b8d3c70c57f7c13e54640
leo60228 <iakornfeld@gmail.com>
2018-11-01 16:48:40 -0400

    init

---
+++ MadikaModule.cs
@@ -0,0 +1,30 @@
+﻿using Celeste.Mod;
+using System;
+using System.Reflection;
+using Celeste;
+
+namespace Madika
+{
+    public class MadikaModule : EverestModule
+    {
+        public static MadikaModule Instance;
+
+        public override Type SettingsType => null;
+
+        public MadikaModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+            On.Celeste.PlayerHair.Render += RenderHair;
+        }
+
+        public override void Unload() {
+            On.Celeste.PlayerHair.Render -= RenderHair;
+        }
+
+        public static void RenderHair(On.Celeste.PlayerHair.orig_Render orig, PlayerHair self) {
+          return;
+        }
+    }
+}
