c96cd99ecc8b443f30dc9e29a554ab2b62a72565
TheFox <krakaka088@gmail.com>
2019-07-19 17:58:16 +0300

    Madika-Plus

--- MadikaModuleSettings.cs
+++ MadikaModuleSettings.cs
@@ -1,38 +1,64 @@
 ﻿using Celeste.Mod;
 using YamlDotNet.Serialization;
 
-namespace Madika {
-    public class MadikaModuleSettings : EverestModuleSettings {
-        public MadikaModuleChar Mode { get; set; } = MadikaModuleChar.Off;
+namespace Madika
+{
+	public class MadikaModuleSettings : EverestModuleSettings
+	{
+		public MadikaModuleChar Mode { get; set; } = MadikaModuleChar.Off;
 
-        [SettingIgnore]
-        [YamlIgnore]
-        public MadikaCharacter Character {
-            get {
-                if (Mode == MadikaModuleChar.Kris || Mode == MadikaModuleChar.Invisible) {
-                    return MadikaModule.Kris;
-                } else if (Mode == MadikaModuleChar.Ralsei) {
-                    return MadikaModule.Ralsei;
-                } else if (Mode == MadikaModuleChar.Monika) {
-                    return MadikaModule.Monika;
-                } else if (Mode == MadikaModuleChar.Niko) {
-                    return MadikaModule.Niko;
-                } else if (Mode == MadikaModuleChar.WorldMachine) {
-                    return MadikaModule.WorldMachine;
-                }
+		[SettingIgnore]
+		[YamlIgnore]
+		public MadikaCharacter Character
+		{
+			get
+			{
+				switch (Mode)
+				{
+					case MadikaModuleChar.Kris:
+					case MadikaModuleChar.Invisible:
+						return MadikaModule.Kris;
+					case MadikaModuleChar.Ralsei:
+						return MadikaModule.Ralsei;
+					case MadikaModuleChar.Monika:
+						return MadikaModule.Monika;
+					case MadikaModuleChar.MonikaHair:
+						return MadikaModule.MonikaHair;
+					case MadikaModuleChar.Niko:
+						return MadikaModule.Niko;
+					case MadikaModuleChar.WorldMachine:
+						return MadikaModule.WorldMachine;
+					case MadikaModuleChar.PirahnaPlant:
+						return MadikaModule.PirahnaPlant;
+					case MadikaModuleChar.O:
+						return MadikaModule.O;
+					case MadikaModuleChar.Kirby:
+						return MadikaModule.Kirby;
+					case MadikaModuleChar.Frosch:
+						return MadikaModule.Frosch;
+					case MadikaModuleChar.Hyper:
+						return MadikaModule.Hyper;
+				}
 
-                return null;
-            }
-        }
-    }
+				return null;
+			}
+		}
+	}
 
-    public enum MadikaModuleChar {
-        Off,
-        Kris,
-        Ralsei,
-        Monika,
-        Niko,
-        WorldMachine,
-        Invisible
-    }
+	public enum MadikaModuleChar
+	{
+		Off,
+		Kris,
+		Ralsei,
+		Monika,
+        MonikaHair,
+		Niko,
+		WorldMachine,
+        PirahnaPlant,
+        O,
+        Kirby,
+        Frosch,
+        Hyper,
+		Invisible
+	}
 }
---
+++ README.md
@@ -0,0 +1,11 @@
+# Madika-Plus
+
+a fork of madika that removes .net dependency and adds more characters and features
+
+# Screenshots
+
+![img1](./docs/img1.png "O in 2A")
+
+![img2](./docs/img2.png "Pirahna Plant in 8A")
+
+![img3](./docs/img3.png "Niko in 4B")
